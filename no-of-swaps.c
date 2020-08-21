3ae16927e7edc778828529f4fce3b57b5072cbff
Tejas <tejaskalpande@gmail.com>
2020-08-21 16:22:57 +0530

    Added sorting using qsort()

--- no-of-swaps.c
+++ no-of-swaps.c
@@ -1,9 +1,33 @@
 /* Find minimum number of swaps to get an array sorted in ascending order */
 
 #include<stdio.h>
+/* For qsort() function */
+#include<stdlib.h>
+
+int compare(const void* x, const void* y)
+{
+	int a = *((int*) x);
+	int b = *((int*) y);
+	/* Return negative if a should be before b, 0 if equal, positive if a should be after b */
+	/* Ascending order */
+	if(a < b)
+		return -1; 
+	if(a > b)
+		return 1;
+	else
+		return 0;
+	/* OR */
+	/*
+	return (a > b) - (a < b);
+	*/
+}
 
 int main()
 {
-	
+	int arr[] = {5, 6, 8, 2, 9, 1, 0, 12};
+	qsort(arr, sizeof(arr)/sizeof(*arr), sizeof(*arr), compare);	
+	for(int i = 0; i < sizeof(arr)/sizeof(*arr); i++)
+		printf("%d ", arr[i]);
+	printf("\n");
 	return 0;
 }
