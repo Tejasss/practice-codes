98814bed02b9cd80d124fa6ade121142b59e1963
Tejas <tejaskalpande@gmail.com>
2020-08-21 19:02:26 +0530

    Completed code

--- no-of-swaps.c
+++ no-of-swaps.c
@@ -1,8 +1,10 @@
-/* Find minimum number of swaps to get an array sorted in ascending order */
-
+/* Find minimum number of swaps to get an array sorted in ascending order (elements in array are distinct) */
+/* Algorithm: https://www.youtube.com/watch?v=f7IIW0HVUcQ */
 #include<stdio.h>
 /* For qsort(), bsearch() functions */
 #include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
 
 int compare(const void* x, const void* y)
 {
@@ -22,20 +24,39 @@ int compare(const void* x, const void* y)
 	*/
 }
 
+int find_cycle(int startIndex, int* arr, int* sorted_arr, bool* visited, int nItems)
+{
+	int noElements = 0;
+	int nextEleIndex = startIndex;
+	do
+	{
+		visited[nextEleIndex] = true;
+		nextEleIndex = (int)((int*)bsearch(&arr[nextEleIndex], sorted_arr, nItems, sizeof(*arr), compare) - sorted_arr);
+		noElements++;
+	}while(arr[nextEleIndex] != arr[startIndex]);
+	return noElements;
+}
+
 int main()
 {
-	int arr[] = {5, 6, 8, 2, 9, 1, 0, 12};
-	qsort(arr, sizeof(arr)/sizeof(*arr), sizeof(*arr), compare);	
-	for(int i = 0; i < sizeof(arr)/sizeof(*arr); i++)
+	int arr[] = {5, 7, 12, 6, 9, 2};
+	int nItems = sizeof(arr)/sizeof(*arr);
+	printf("Entered array is ");
+	for(int i = 0; i<nItems; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
-	int item = 6;
-	int *pItem = NULL;
-	pItem = (int*) bsearch(&item, arr, sizeof(arr)/sizeof(*arr), sizeof(*arr), compare);
-	if(pItem)
-		printf("Found at index %ld\n", pItem - arr);
-	else
-		printf("Not found\n");
-	
+	int* sorted_arr = (int*) malloc(sizeof(arr));
+	memcpy(sorted_arr, arr, sizeof(arr));	
+	qsort(sorted_arr, nItems, sizeof(*arr), compare);
+	bool* visited = (bool*) calloc(nItems, sizeof(bool));
+	int noSwaps = 0, noElements = 0;
+	for(int i = 0; i<nItems; i++)
+	{
+		if(visited[i] == true)
+			continue;
+		noElements = find_cycle(i, arr, sorted_arr, visited, nItems);
+		noSwaps += (noElements - 1);
+	}
+	printf("Minimum number of swaps to sort the array = %d\n", noSwaps);
 	return 0;
 }
